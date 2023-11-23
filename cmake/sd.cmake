13038692441ce64ebbd896ff0187ffea8bf7f040
Cyberhan123 <255542417@qq.com>
2023-11-23 21:40:14 +0800

    update sd version

--- cmake/sd.cmake
+++ cmake/sd.cmake
@@ -4,7 +4,7 @@ if(${CMAKE_VERSION} VERSION_LESS 3.14)
     include(add_FetchContent_MakeAvailable.cmake)
 endif()
 
-set(SD_GIT_TAG  fb7d9217bb5dd6ac82b4fb1f312381f63b65ba1a)
+set(SD_GIT_TAG  46eacf7fa133e98df6f51b769e4bb68a48ff3fa3)
 set(SD_GIT_URL  https://github.com/Cyberhan123/stable-diffusion.cpp)
 
 FetchContent_Declare(
