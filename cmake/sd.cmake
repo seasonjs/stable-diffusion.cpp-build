9216a2bfa3d5d173df1db00188519a90fc2104e5
Cyberhan123 <255542417@qq.com>
2023-11-27 21:49:42 +0800

    clean up CMakeLists.txt

--- cmake/sd.cmake
+++ cmake/sd.cmake
@@ -6,11 +6,11 @@ endif()
 
 set(SD_GIT_TAG  1ac4ad2ba21c7f1c96c061f452bde129522a85c1)
 set(SD_GIT_URL  https://github.com/Cyberhan123/stable-diffusion.cpp)
+set(BUILD_SHARED_LIBS OFF)
 
 FetchContent_Declare(
   sd
   GIT_REPOSITORY    ${SD_GIT_URL}
   GIT_TAG           ${SD_GIT_TAG}
 )
-
 FetchContent_MakeAvailable(sd)
