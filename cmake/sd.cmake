eefcc62875902fbb1e9a0cde3651676c61e7fe0f
Cyberhan123 <255542417@qq.com>
2023-12-25 20:52:56 +0800

    sync latest version

--- cmake/sd.cmake
+++ cmake/sd.cmake
@@ -4,7 +4,7 @@ if(${CMAKE_VERSION} VERSION_LESS 3.14)
     include(add_FetchContent_MakeAvailable.cmake)
 endif()
 
-set(SD_GIT_TAG  ac8f5a044c762eaa1181a89ef673fcb174acfb47)
+set(SD_GIT_TAG  0e64238e4c4c902e0c043b741ae48fe22a2fd0fa)
 set(SD_GIT_URL  https://github.com/leejet/stable-diffusion.cpp)
 set(BUILD_SHARED_LIBS OFF)
 
