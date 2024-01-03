e11a571695d78939adfc59e1480d385927a8686e
Cyberhan123 <255542417@qq.com>
2024-01-03 19:39:06 +0800

    fix sysmbol with util and upscaler

--- stable-diffusion-abi.cpp
+++ stable-diffusion-abi.cpp
@@ -17,3 +17,37 @@ uint32_t get_image_height(const sd_image_t* images, int index) {
 uint32_t get_image_channel(const sd_image_t* images, int index) {
     return images[index].channel;
 }
+
+void sd_images_free(const sd_image_t* images) {
+    if (images != nullptr) {
+        delete []images;
+    }
+    images = nullptr;
+}
+
+void sd_image_free(sd_image_t* image) {
+    if (image != nullptr) {
+        delete image;
+    }
+    image = nullptr;
+}
+
+// sd_image_t new_image() {
+//
+// }
+//
+// void set_image_data(sd_image_t image, uint8_t* data) {
+//     image.data = data;
+// }
+//
+// void set_image_width(sd_image_t image, uint32_t width) {
+//     image.width = width;
+// }
+//
+// void set_image_height(sd_image_t image, uint32_t height) {
+//     imageheight = height;
+// }
+//
+// void set_image_channel(sd_image_t image, uint32_t channel) {
+//     image->channel = channel;
+// }
--- stable-diffusion-abi.h
+++ stable-diffusion-abi.h
@@ -21,6 +21,14 @@
 extern "C" {
 #endif
 
+// STABLE_DIFFUSION_API void set_image_data(sd_image_t* image, uint8_t* data);
+//
+// STABLE_DIFFUSION_API void set_image_width(sd_image_t* image, uint32_t width);
+//
+// STABLE_DIFFUSION_API void set_image_height(sd_image_t* image, uint32_t height);
+//
+// STABLE_DIFFUSION_API void set_image_channel(sd_image_t* image, uint32_t channel);
+
 STABLE_DIFFUSION_API uint8_t* get_image_data(const sd_image_t* images, int index);
 
 STABLE_DIFFUSION_API uint32_t get_image_width(const sd_image_t* images, int index);
@@ -28,6 +36,10 @@ STABLE_DIFFUSION_API uint32_t get_image_width(const sd_image_t* images, int inde
 STABLE_DIFFUSION_API uint32_t get_image_height(const sd_image_t* images, int index);
 
 STABLE_DIFFUSION_API uint32_t get_image_channel(const sd_image_t* images, int index);
+
+STABLE_DIFFUSION_API void sd_images_free(const sd_image_t* images);
+
+STABLE_DIFFUSION_API void sd_image_free(sd_image_t* image);
 
 #ifdef __cplusplus
 }
